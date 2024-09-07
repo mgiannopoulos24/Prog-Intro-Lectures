27dfaa956d2e50ea2a8cf5a4d79b0a14492a03ed
mgiannopoulos24 <mariosgian_2002@yahoo.gr>
2024-09-07 19:20:59 +0300

    Initial commit

---
+++ src/components/iconMap.js
@@ -0,0 +1,9 @@
+import wave from '../assets/wave.png';
+import matrix from '../assets/mathematics.png';
+import pointer from '../assets/return.png';
+
+export const iconMap = {
+    'wave': wave,
+    'matrix': matrix,
+    'pointer': pointer
+};
