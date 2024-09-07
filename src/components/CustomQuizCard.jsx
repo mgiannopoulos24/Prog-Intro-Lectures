5643413c6d70096905e3757b043ea66d4c0e3046
matinanadali <matinanad@gmail.com>
2024-09-07 17:45:27 +0300

    Added quiz cards

---
+++ src/components/CustomQuizCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./CustomQuizCard.css";
+import { Button } from '@mui/material'; 
+
+const QuizCard = ({ image, title, description, onClick }) => {
+  return (
+    <div className="quiz-card">
+      <div className="quiz-img-container">
+        <img src={require("../assets/"+image)} alt="quiz-img" className="quiz-img" />
+      </div>
+      <div className="quiz-title">
+        <div className='quiz-info'>
+          <h2>{title}</h2>
+          <h3>{description}</h3>
+        </div>
+        <div className="play-button-container">
+        <Button variant="contained" className="play-button" onClick={onClick}>
+          Play!
+        </Button>
+      </div>
+      </div>
+      
+    </div>
+  );
+};
+
+export default QuizCard;
--- src/pages/Quizzes.jsx
+++ src/pages/Quizzes.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import './styles/Quizzes.css';
+import QuizzesCardData from '../QuizzesCardData';
+import CustomQuizCard from '../components/CustomQuizCard';
 
 const Quizzes = () => {
-    return (
-        <div>
 
-        </div>
+    const handleQuizCardClick = (cardId) => {
+        return;
+    }
+
+    return (
+        <>
+            <div className="Quizzes container mt-5">
+                <div className="text-center">
+                    <h1>Kahoot Quizzes</h1>
+                    <h3>Time for some fun!</h3>
+                    <hr className="my-4" />
+                </div>      
+            </div>
+            <div className="cards d-flex justify-content-center flex-wrap gap-3">
+                {QuizzesCardData.map((card) => (
+                    <CustomQuizCard
+                    image={card.image}
+                    title={card.title}
+                    description={card.description}
+                    onClick={() => handleQuizCardClick(card.id)}
+                    />
+                ))}
+            </div>
+        </> 
     )
 }
 
