d40f630e9429b2be458ef98c8c8d5c30c2f50cb2
mgiannopoulos24 <mariosgian_2002@yahoo.gr>
2024-09-09 17:57:59 +0300

    Add ToggleSwitch component to App.jsx and implement dark mode styles in index.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import './App.css';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Quizzes from './pages/Quizzes';
+import ToggleSwitch from './components/buttons/ToggleSwitch';
 
 function App() {
   return (
     <>
+    <ToggleSwitch />
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
