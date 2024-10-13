5fd0a384205109ab18ae60b5ad9bf4755566c188
mgiannopoulos24 <mariosgian_2002@yahoo.gr>
2024-10-14 00:28:45 +0300

    initial commit of q3 and q4

--- src/components/dialogs/images.js
+++ src/components/dialogs/images.js
@@ -16,10 +16,37 @@ import q2q7 from '../../assets/q2q7.jpg';
 import q2q8 from '../../assets/q2q8.jpg';
 import q2q9 from '../../assets/q2q9.jpg';
 import q2q10 from '../../assets/q2q10.jpg';
+import q3q1 from '../../assets/q3q1.png';
+import q3q2 from '../../assets/q3q2.jpg';
+import q3q3 from '../../assets/q3q3.jpg';
+import q3q4 from '../../assets/q3q4.jpg';
+import q3q5 from '../../assets/q3q5.png';
+import q3q6 from '../../assets/q3q6.png';
+import q3q7 from '../../assets/q3q7.jpg';
+import q3q8 from '../../assets/q3q8.jpg';
+import q3q9 from '../../assets/q3q9.jpg';
+import q3q10 from '../../assets/q3q10.jpg';
+import q3q11 from '../../assets/q3q11.jpg';
+import q3q12 from '../../assets/q3q12.jpg';
+import q3q13 from '../../assets/q3q13.jpg';
+import q3q14 from '../../assets/q3q14.jpg';
+import q3q15 from '../../assets/q3q15.png';
+import q4q1 from '../../assets/q4q1.jpg';
+import q4q2 from '../../assets/q4q2.png';
+import q4q3 from '../../assets/q4q3.jpg';
+import q4q4 from '../../assets/q4q4.jpg';
+import q4q5 from '../../assets/q4q5.jpg';
+import q4q6 from '../../assets/q4q6.jpg';
+import q4q7 from '../../assets/q4q7.jpg';
+import q4q8 from '../../assets/q4q8.jpg';
+import q4q9 from '../../assets/q4q9.jpg';
+import q4q10 from '../../assets/q4q10.jpg';
 
 const images = {
     "q1q1": q1q1, "q1q2": q1q2, "q1q3": q1q3, "q1q4": q1q4, "q1q5": q1q5, "q1q6": q1q6, "q1q7": q1q7, "q1q8": q1q8,
-    "q2q1": q2q1, "q2q2": q2q2, "q2q3": q2q3, "q2q4": q2q4, "q2q5": q2q5, "q2q6": q2q6, "q2q7": q2q7, "q2q8": q2q8, "q2q9": q2q9, "q2q10": q2q10
+    "q2q1": q2q1, "q2q2": q2q2, "q2q3": q2q3, "q2q4": q2q4, "q2q5": q2q5, "q2q6": q2q6, "q2q7": q2q7, "q2q8": q2q8, "q2q9": q2q9, "q2q10": q2q10,
+    "q3q1": q3q1, "q3q2": q3q2, "q3q3": q3q3, "q3q4": q3q4, "q3q5": q3q5, "q3q6": q3q6, "q3q7": q3q7, "q3q8": q3q8, "q3q9": q3q9, "q3q10": q3q10, "q3q11": q3q11, "q3q12": q3q12, "q3q13": q3q13, "q3q14": q3q14, "q3q15": q3q15,
+    "q4q1": q4q1, "q4q2": q4q2, "q4q3": q4q3, "q4q4": q4q4, "q4q5": q4q5, "q4q6": q4q6, "q4q7": q4q7, "q4q8": q4q8, "q4q9": q4q9, "q4q10": q4q10
 };
 
 export default images;
