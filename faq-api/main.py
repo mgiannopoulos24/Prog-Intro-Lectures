346baa86ffd20c743958f0b0643dfc979c276f83
mgiannopoulos24 <79588074+mgiannopoulos24@users.noreply.github.com>
2025-11-15 12:01:13 +0200

    Add methods

--- faq-api/main.py
+++ faq-api/main.py
@@ -40,7 +40,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[FRONTEND_URL],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
 )
 
